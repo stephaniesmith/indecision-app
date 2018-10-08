26364345d7a3571f5a3ac47b2d1ca23c5a8cc830
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-10-08 10:19:14 -0700

    second template

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,10 +3,53 @@
 console.log('app.js hello world');
 
 var template = React.createElement(
-  'h1',
+  'div',
   null,
-  'Indecision App'
+  React.createElement(
+    'h1',
+    null,
+    'Indecision App'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'This is some text'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
+);
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Stephanie Smith'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: 28'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: Portland'
+  )
 );
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 console.log('app.js hello world');
 
-var template = <h1>Indecision App</h1>;
+var template = (
+  <div>
+    <h1>Indecision App</h1>
+    <p>This is some text</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
+
+var templateTwo = (
+  <div>
+    <h1>Stephanie Smith</h1>
+    <p>Age: 28</p>
+    <p>Location: Portland</p>
+  </div>
+)
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
