f327558c4fe5ef89b2bc16c092abab4756988fd5
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-10-12 11:59:43 -0700

    more basic component work

--- src/app.js
+++ src/app.js
@@ -1,19 +1,41 @@
-class Header extends React.Component {
+class IndecisionApp extends React.Component {
   render() {
+    const title = 'Indecision';
+    const subTitle = 'Decide some stuff.';
+    const options = ['Stuff 1', 'Stuff 2', 'Stuff 3'];
+
     return (
       <div>
-        <h1>Indecision</h1>
-        <h2>Decide some stuff.</h2>
+        <Header title={title} subTitle={subTitle}/>
+        <Action/>
+        <Options options={options}/>
+        <AddOption/>
+      </div>
+    )
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    const { title, subTitle } = this.props;
+    return (
+      <div>
+        <h1>{title}</h1>
+        <h2>{subTitle}</h2>
       </div>
     )
   }
 }
 
 class Action extends React.Component {
+  handlePick() {
+    alert('handle')
+  }
+
   render() {
     return (
       <div>
-        <button>What should I do?</button>
+        <button onClick={this.handlePick}>What should I do?</button>
       </div>
     )
   }
@@ -21,9 +43,22 @@ class Action extends React.Component {
 
 class Options extends React.Component {
   render() {
+    const { options } = this.props;
+
     return (
       <div>
-        Options components Here
+        {options.map((option, index) => <Option key={index} option={option}/>)}
+      </div>
+    )
+  }
+}
+
+class Option extends React.Component {
+  render() {
+    const { option } = this.props;
+    return (
+      <div>
+        {option}
       </div>
     )
   }
@@ -39,13 +74,4 @@ class AddOption extends React.Component {
   }
 }
 
-const jsx = (
-  <div>
-    <Header/>
-    <Action/>
-    <Options/>
-    <AddOption/>
-  </div>
-);
-
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
