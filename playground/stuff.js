113d97d12287f1e3896f46620f59c8011336b4b2
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-10-12 11:10:49 -0700

    restructure

---
+++ playground/stuff.js
@@ -0,0 +1,58 @@
+console.log('app.js hello world');
+
+const app = {
+  title: 'Indecision App',
+  subTitle: 'This is some text',
+  options: []
+}
+
+const onFormSubmit = e => {
+  e.preventDefault();
+
+  const option = e.target.elements.option.value;
+
+  if(option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    render();
+  }
+} 
+
+const removeAll = () => {
+  app.options = [];
+  render();
+}
+
+const onMakeDecision = () => {
+  const randomNum = Math.floor(Math.random() * app.options.length);
+  const option = app.options[randomNum];
+  alert(option);
+}
+
+const appRoot = document.getElementById('app');
+
+const render = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subTitle && <p>{app.subTitle}</p>}
+      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+      
+      <button disabled={app.options.length === 0}onClick={onMakeDecision}>What should I do?</button>
+      <button onClick={removeAll}>Remove All</button>
+      <ol>
+        {
+          app.options.map((option, index) => <li key={index}>{option}</li>)
+        }  
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option"/>
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
+
+  ReactDOM.render(template, appRoot);
+}
+
+render()
