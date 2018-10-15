b897b6465b1a45aa255522d7cc756a4b5b799ac0
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-10-15 09:35:13 -0700

    starting to work on the modal

--- src/app.js
+++ src/app.js
@@ -2,28 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-const Layout = props => {
-  return (
-    <div>
-      <header>Header</header>
-      {props.children}
-      <footer>Footer</footer>
-    </div>
-  )
-};
-
-const template = (
-  <div>
-    <h1>Page Title</h1>
-    <p>This is my page</p>
-  </div>
-)
-
-ReactDOM.render((
-  <Layout>
-    <div>
-      <h1>Page Title</h1>
-      <p>This is my page</p>
-    </div>  
-  </Layout>
-), document.getElementById('app'));
+ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -3,6 +3,7 @@ import AddOption from './AddOption'
 import Options from './Options'
 import Header from './Header'
 import Action from './Action'
+import OptionModal from './OptionModal'
 
 export default class IndecisionApp extends React.Component {
 
@@ -84,6 +85,7 @@ export default class IndecisionApp extends React.Component {
         <AddOption
           handleAddOption={this.handleAddOption}
         />
+        <OptionModal/>
       </div>
     )
   }
