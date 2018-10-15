4bbf92afbcfe5a86d43ca0367d91391f356255b5
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-10-15 09:19:49 -0700

    children property

--- src/app.js
+++ src/app.js
@@ -1,5 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp'
+import IndecisionApp from './components/IndecisionApp';
 
-ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
+const Layout = props => {
+  return (
+    <div>
+      <header>Header</header>
+      {props.children}
+      <footer>Footer</footer>
+    </div>
+  )
+};
+
+const template = (
+  <div>
+    <h1>Page Title</h1>
+    <p>This is my page</p>
+  </div>
+)
+
+ReactDOM.render((
+  <Layout>
+    <div>
+      <h1>Page Title</h1>
+      <p>This is my page</p>
+    </div>  
+  </Layout>
+), document.getElementById('app'));
