1a521dedc124ee21728f5dcb785ebad035cc66bd
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-10-15 15:11:46 -0700

    styling the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ const Header = props => {
   const { title, subTitle } = props;
   
   return (
-    <div>
-      <h1>{title}</h1>
-      {subTitle && <h2>{subTitle}</h2>}
+    <div className="header">
+      <h1 className="header__title">{title}</h1>
+      {subTitle && <h2 className="header__subtitle">{subTitle}</h2>}
     </div>
   )
 }
